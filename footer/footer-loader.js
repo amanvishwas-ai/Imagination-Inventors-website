5a7afacdacd1afab50b70c8db26014e447ec5729
Imagination-Inventors <imaginationinventors@gmail.com>
2026-03-15 11:52:21 +0530

    proper add .

---
+++ footer/footer-loader.js
@@ -0,0 +1,10 @@
+async function loadFooter() {
+
+  const response = await fetch("../footer/footer.html");
+  const html = await response.text();
+
+  document.getElementById("footer-placeholder").innerHTML = html;
+
+}
+
+loadFooter();
