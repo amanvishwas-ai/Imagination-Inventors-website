2a005a261bff009791c0abc62c3de80cd4126ef6
Imagination-Inventors <imaginationinventors@gmail.com>
2026-03-15 20:14:12 +0530

    refactor(project-navbar): make the navbar for project pages dynamic and injectable

---
+++ project-navbar/project-navbar-loader.js
@@ -0,0 +1,23 @@
+async function loadNavbar(){
+
+  const res = await fetch("/project-navbar/project-navbar.html");
+  document.getElementById("navbar-placeholder").innerHTML = await res.text();
+
+  initNavbar();
+}
+
+function initNavbar(){
+
+  const hamburger = document.querySelector("#hamburger");
+  const navLinks = document.querySelector("#navLinks");
+  const overlay = document.querySelector("#menuOverlay");
+
+  hamburger.onclick = () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+    overlay.classList.toggle("active");
+  };
+
+}
+
+loadNavbar();
