be64c5087b33502744472c69a05cecdeba9a3371
Imagination-Inventors <imaginationinventors@gmail.com>
2026-03-15 13:30:17 +0530

    test

--- footer/footer-loader.js
+++ footer/footer-loader.js
@@ -1,6 +1,6 @@
 async function loadFooter() {
 
-  const response = await fetch("../footer/footer.html");
+  const response = await fetch("footer/footer.html");
   const html = await response.text();
 
   document.getElementById("footer-placeholder").innerHTML = html;
